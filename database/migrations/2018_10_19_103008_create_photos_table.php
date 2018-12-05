9c6a0d1c0f1561f2100d45690cdfacf116f39f29
Sergey Khrulev <sergey.hrulev@gmail.com>
2018-12-05 14:31:42 +0300

    add_column to photos

--- database/migrations/2018_10_19_103008_create_photos_table.php
+++ database/migrations/2018_10_19_103008_create_photos_table.php
@@ -15,6 +15,7 @@ class CreatePhotosTable extends Migration
     {
         Schema::create('photos', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id')->unsigned();
             $table->integer('bullet_id')->unsigned();
             $table->string('preview');
             $table->string('photo');
